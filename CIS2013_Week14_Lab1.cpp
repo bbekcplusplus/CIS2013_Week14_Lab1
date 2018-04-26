1522d1b980d7b40b3c6e9f5c7871c33e62d96210
Bibek Gupta <bibek.gupta@okstate.edu>
2018-04-25 19:29:54 -0500

    Initial commit

---
+++ CIS2013_Week14_Lab1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	chatr next = 'y';
+
+	while (next != 'n')
+	{
+		cout << "Add to list(a)" << endl;
+		cout << "Done list item(d)" << endl;
+		cout << "Print list(p)" << endl;
+		cout << "Exit list app(x)" << endl;
+		cout << "What do you want to do?" << endl;
+		cin >> next;
+		switch (next)
+		{
+		case'a':
+			//add stuff to list
+			break;
+		case'd':
+			//finish something in list
+			break;
+		case'p':
+			//print list
+			break;
+		case 'n':
+			//All done with to do list
+		}
+	}
+}
